9c39a073da498da061d532793bd3def0526c070f
MUKESH <mukesh1992rajbhar@gmail.com>
2024-09-01 22:14:08 +0530

    add new code

--- app/src/main/java/com/example/practiseproject/MainActivity.kt
+++ app/src/main/java/com/example/practiseproject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.practiseproject
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,10 +11,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.practiseproject.ui.theme.PractiseProjectTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -41,7 +44,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    val context = LocalContext.current
     PractiseProjectTheme {
         Greeting("Android")
+        Toast.makeText(context, "j'hmrwlhklrw", Toast.LENGTH_SHORT).show()
     }
 }
